5e4dbe2ee66f4acecae43a6edc0d3d97c69c6163
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-02-02 12:12:22 -0800

    created autosearch component skeleton and loaded into app component

---
+++ autosearch/src/components/Autosearch.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Autosearch extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Autocomplete Search</h1>
+                <input />
+                <ul>
+                    <li>first item</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Autosearch;
